342975688e4991dba85e1819d315208d283066cd
richtestani <richtestani@mac.com>
2021-02-23 18:53:51 -0500

    Update pre-payment-webhook-webflow.js

--- src/functions/pre-payment-webhook-webflow/pre-payment-webhook-webflow.js
+++ src/functions/pre-payment-webhook-webflow/pre-payment-webhook-webflow.js
@@ -161,7 +161,7 @@ function getCountryCurrency(items) {
     //console.log(items._embedded['fx:item_options']);
 
     let item = items;
-    console.log(JSON.stringify(item._links));
+    console.log(JSON.stringify(item['_links']));
     let currency = getOption(item, 'currency');
     customClientData.currency = currency;
     console.log(customClientData);
